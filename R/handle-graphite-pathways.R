44befc6a8108b268e4d8a1b667b8ea216fdb5391
Paolo Martini <paolo.cavei@gmail.com>
2018-05-23 11:56:32 +0200

    New examples for the function

--- R/handle-graphite-pathways.R
+++ R/handle-graphite-pathways.R
@@ -7,6 +7,11 @@
 #'
 #' @return a data frame, id and pathway name
 #'
+#' @examples
+#'   if (require(graphite)){
+#'     pathways <- pathways("hsapiens", "kegg")
+#'     mapPathwaysIDfromGraphite(pathways, names(pathways)[1:10])
+#'   }
 #' @importFrom checkmate assertClass
 #'
 #' @export
